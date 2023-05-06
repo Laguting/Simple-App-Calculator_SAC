1472f73be42cab6b6f27dfa90e472f572e1e506f
Laguting <lagutingmariconjaneg@gmail.com>
2023-05-06 15:53:40 +0800

    Initial display

--- SAC.py
+++ SAC.py
@@ -2,6 +2,14 @@
 # Task: Create a Simple App Calculator that applies the Exception handling lesson.
 
 # Opening
+from pyfiglet import Figlet
+from termcolor import colored
+s_a_c = Figlet(font = "digital")
+print()
+print("⋆｡ﾟ☁︎｡⋆｡ ﾟ☾ ﾟ｡⋆" * 10)
+print()
+print(colored(s_a_c.renderText("Simple App Calculator"), "yellow"))
+print("⋆｡ﾟ☁︎｡⋆｡ ﾟ☾ ﾟ｡⋆" * 10)
 # Welcome
   # Ask the user for any inputs
   # Perform the calculations
