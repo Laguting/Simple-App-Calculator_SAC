fa82f9377ea8824078d5fe85802bae33c9654a71
Laguting <lagutingmariconjaneg@gmail.com>
2023-05-06 19:19:40 +0800

    The calculator history works and the add operation

--- SAC.py
+++ SAC.py
@@ -22,14 +22,21 @@ print("          ┻┳|")
 print()
 print("⋆｡ﾟ☁︎｡⋆｡ ﾟ☾ ﾟ｡⋆" * 10)
   # Ask the user for any inputs
-while True:
-    ask_usr = input("\n\n\33[1m\33[34m What operation would you like to perform? Enter Add = '+'; Subtraction = '-'; Multiplication = '*'; Division = '/': \33[0m")
-    try:
-        number_1 = float(input("1st number: "))
-        number_2 = float(input("2nd number: "))
-    except ValueError:
-        print("This calculator only accepts numbers") 
+with open("calc_history.text", "w") as calculatorhistory_file: # Open text file that will hold the history of inputs
+    while True:
+        ask_usr = input("\n\n\33[1m\33[34m What operation would you like to perform? Enter Add = '+'; Subtraction = '-'; Multiplication = '*'; Division = '/': \33[0m")
+        try:
+            number_1 = float(input("\n\33[43m1st number: \33[0m"))
+            number_2 = float(input("\n\33[43m2nd number: \33[0m"))
+        except ValueError:
+            print("\n\33[1m\33[31mThis calculator only accepts numbers\33[0m") 
   # Perform the calculations
+        if ask_usr == "+":
+            add = number_1 + number_2
+            result1 = (f"\n\33[7m{number_1} {ask_usr} {number_2} = {add}\33[0m")
+            print(result1)
+            calculatorhistory_file.write(f"{number_1} {ask_usr} {number_2} = {add}" + '\n')
+        calculatorhistory_file.close()
   # ask for another inputs
      # If no, Display "Thank you" and exit
 # Closing
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -20,17 +20,22 @@ print("          ┻┳|•.•) i'm watching.....")
 print("          ┳┻|  ⊂ﾉ)")
 print("          ┻┳|")
 print()
+print("⋆｡ﾟ☁︎｡⋆｡ ﾟ☾ ﾟ｡⋆" * 10)
   # Ask the user for any inputs
-def calculator():
-    ask_usr = input("What operation would you like to perform? Enter Add = '+'; Subtraction = '-'; Multiplication = '*'; Division = '/': ")
-    print(ask_usr)
-    try:
-        number_1 = float(input("1st number: "))
-        number_2 = float(input("2nd number: "))
-    except:
-        print("This calculator only accepts numbers") 
+with open("Calculator_History.txt", "w") as calculatorhistory_file: # Open text file that will hold the history of inputs
+    while True:
+        ask_usr = input("\n\n\33[1m\33[34m What operation would you like to perform? Enter Add = '+'; Subtraction = '-'; Multiplication = '*'; Division = '/': \33[0m")
+        try:
+            number_1 = float(input("\n\33[43m1st number: \33[0m"))
+            number_2 = float(input("\n\33[43m2nd number: \33[0m"))
+        except ValueError:
+            print("This calculator only accepts numbers") 
   # Perform the calculations
+        if ask_usr == "+":
+            add = number_1 + number_2
+            result1 = (f"\n{number_1} {ask_usr} = {add}")
+            print(result1)
+            calculatorhistory_file.write(result1 + '\n')
   # ask for another inputs
-     # If yes
      # If no, Display "Thank you" and exit
 # Closing
