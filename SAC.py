6ccc732ed143c299b9f49108c615d75862335265
Laguting <lagutingmariconjaneg@gmail.com>
2023-05-06 21:23:25 +0800

    Added the question for the user and adjust some indentions

--- SAC.py
+++ SAC.py
@@ -14,17 +14,21 @@ print("⋆｡ﾟ☁︎｡⋆｡ ﾟ☾ ﾟ｡⋆" * 10)
 s_a_c = Figlet(font = "banner3-D")
 print()
 print(colored(s_a_c.renderText(" Welcome"), "green"))
-print("          ┻┳|              ")
-print("          ┳┻| _")
-print("          ┻┳|•.•) i'm watching.....")
-print("          ┳┻|  ⊂ﾉ)")
-print("          ┻┳|")
+print("\33[35m          ┻┳|              \33[0m")
+print("\33[35m          ┳┻| _\33[0m")
+print("\33[35m          ┻┳|•.•) i'm watching.....\33[0m")
+print("\33[35m          ┳┻|  ⊂ﾉ)\33[0m")
+print("\33[35m          ┻┳|\33[0m")
 print()
 print("⋆｡ﾟ☁︎｡⋆｡ ﾟ☾ ﾟ｡⋆" * 10)
   # Ask the user for any inputs
 def process():
     with open("calc_history.text", "w") as calculatorhistory_file: # Open text file that will hold the history of inputs
         while True:
+     # ask for another inputs
+          again_usr = input("\n\33[36m Would you like to try again: Enter 'Y' if yes and 'N' if no: \33[0m")
+     # If yes call process
+          if again_usr.upper() == "Y":
             try:
                ask_usr = input("\n\n\33[1m\33[34m What operation would you like to perform? Enter Add = '+'; Subtraction = '-'; Multiplication = '*'; Division = '/': \33[0m")
                if ask_usr not in ["+", "-", "*", "/"]:
@@ -55,7 +59,6 @@ def process():
                 print("\n", result2, "\n")
                 print("⋆｡ﾟ☁︎｡⋆｡ ﾟ☾ ﾟ｡⋆" * 10)
                 calculatorhistory_file.write(f"{number_1} {ask_usr} {number_2} = {sub_tract}" + '\n')
-
     # Multiplication
             elif ask_usr == "*":
                 multi_ply = number_1 * number_2
@@ -72,8 +75,13 @@ def process():
                 print("\n", result4, "\n")
                 print("⋆｡ﾟ☁︎｡⋆｡ ﾟ☾ ﾟ｡⋆" * 10)
                 calculatorhistory_file.write(f"{number_1} {ask_usr} {number_2} = {divi_sion}" + '\n')
-            calculatorhistory_file.close()            
-process()
-  # ask for another inputs
+            else:
+                break
+            calculatorhistory_file.close()
      # If no, Display "Thank you" and exit
+          else:
+            exit_1 = Figlet(font = "slant")
+            print(colored(exit_1.renderText("Thank you! <3"), "magenta"))
+            print("\n\33[3m Until next time!...\33[0m")            
 # Closing
+process()
