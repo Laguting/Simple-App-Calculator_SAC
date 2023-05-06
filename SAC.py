38a1c4b19d4861fa75b4f15a2fd102df3a84bee6
Laguting <lagutingmariconjaneg@gmail.com>
2023-05-06 10:32:53 +0800

    Tentative Pseudocodes

---
+++ SAC.py
@@ -0,0 +1,11 @@
+# Laguting, Maricon Jane G.
+# Task: Create a Simple App Calculator that applies the Exception handling lesson.
+
+# Opening
+# Welcome
+  # Ask the user for any inputs
+  # Perform the calculations
+  # ask for another inputs
+     # If yes
+     # If no, Display "Thank you" and exit
+# Closing
