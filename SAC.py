02c27437550ecbe424e43da2800dc2dcf792df3c
Laguting <lagutingmariconjaneg@gmail.com>
2023-05-09 21:41:39 +0800

    Made some revisions according to Prof. Madrigalejos wherein the question for the user if they want to retry is displayed after inputting the first equation. However in here the Calculator history, will be replaced immediately after entering the second equation.

--- SAC.py
+++ SAC.py
@@ -38,64 +38,76 @@ print()
 def process():
     with open("calc_history.text", "w") as calculatorhistory_file: # Open text file that will hold the history of inputs
         while True:
-     # ask if they wanted to continue
-          again_usr = input("\n\33[36m Would you like to try again: Enter 'Y' if yes and 'N' if no: \33[0m")
-     # If yes call process
-          if again_usr.upper() == "Y":
-             try:
+            try:
                ask_usr = input("\n\n\33[1m\33[34m What operation would you like to perform? Enter Add = '+'; Subtraction = '-'; Multiplication = '*'; Division = '/': \33[0m")
                if ask_usr not in ["+", "-", "*", "/"]:
                    raise ValueError
-             except ValueError:
-                print("\n\33[1mInvalid Operation. Try again.\33[0m")
-                process()
-             try:
-                number_1 = float(input("\n\33[43m1st number: \33[0m"))
-                number_2 = float(input("\n\33[43m2nd number: \33[0m"))
-             except ValueError:
-                print("\n\33[1m\33[31mThis calculator only accepts numbers\33[0m") 
-                process()
+            except ValueError:
+               print("\n\33[1mInvalid Operation. Try again.\33[0m")
+               process()
+            try:
+               number_1 = float(input("\n\33[43m1st number: \33[0m"))
+               number_2 = float(input("\n\33[43m2nd number: \33[0m"))
+            except ValueError:
+               print("\n\33[1m\33[31mThis calculator only accepts numbers\33[0m") 
+               process()
   # Perform the calculations
     # Add
-             if ask_usr == "+": 
-                add = number_1 + number_2
-                result1 = (f"\n\33[7m{number_1} {ask_usr} {number_2} = {add}\33[0m")
-                print("⋆｡ﾟ☁︎｡⋆｡ ﾟ☾ ﾟ｡⋆" * 10)
-                print("\n", result1, "\n")
-                print("⋆｡ﾟ☁︎｡⋆｡ ﾟ☾ ﾟ｡⋆" * 10)
-                calculatorhistory_file.write(f"{number_1} {ask_usr} {number_2} = {add}" + '\n')
+            if ask_usr == "+": 
+               add = number_1 + number_2
+               result1 = (f"\n\33[7m{number_1} {ask_usr} {number_2} = {add}\33[0m")
+               print("⋆｡ﾟ☁︎｡⋆｡ ﾟ☾ ﾟ｡⋆" * 10)
+               print("\n", result1, "\n")
+               print("⋆｡ﾟ☁︎｡⋆｡ ﾟ☾ ﾟ｡⋆" * 10)
+               calculatorhistory_file.write(f"{number_1} {ask_usr} {number_2} = {add}" + '\n')
+               break
     # Subtract
-             elif ask_usr == "-":
-                sub_tract = number_1 - number_2
-                result2 = (f"\n\33[7m{number_1} {ask_usr} {number_2} = {sub_tract}\33[0m")
-                print("⋆｡ﾟ☁︎｡⋆｡ ﾟ☾ ﾟ｡⋆" * 10)
-                print("\n", result2, "\n")
-                print("⋆｡ﾟ☁︎｡⋆｡ ﾟ☾ ﾟ｡⋆" * 10)
-                calculatorhistory_file.write(f"{number_1} {ask_usr} {number_2} = {sub_tract}" + '\n')
+            elif ask_usr == "-":
+               sub_tract = number_1 - number_2
+               result2 = (f"\n\33[7m{number_1} {ask_usr} {number_2} = {sub_tract}\33[0m")
+               print("⋆｡ﾟ☁︎｡⋆｡ ﾟ☾ ﾟ｡⋆" * 10)
+               print("\n", result2, "\n")
+               print("⋆｡ﾟ☁︎｡⋆｡ ﾟ☾ ﾟ｡⋆" * 10)
+               calculatorhistory_file.write(f"{number_1} {ask_usr} {number_2} = {sub_tract}" + '\n')
+               break
     # Multiplication
-             elif ask_usr == "*":
-                multi_ply = number_1 * number_2
-                result3 = (f"\n\33[7m{number_1} {ask_usr} {number_2} = {multi_ply}\33[0m")
-                print("⋆｡ﾟ☁︎｡⋆｡ ﾟ☾ ﾟ｡⋆" * 10)
-                print("\n", result3, "\n")
-                print("⋆｡ﾟ☁︎｡⋆｡ ﾟ☾ ﾟ｡⋆" * 10)
-                calculatorhistory_file.write(f"{number_1} {ask_usr} {number_2} = {multi_ply}" + '\n')
+            elif ask_usr == "*":
+               multi_ply = number_1 * number_2
+               result3 = (f"\n\33[7m{number_1} {ask_usr} {number_2} = {multi_ply}\33[0m")
+               print("⋆｡ﾟ☁︎｡⋆｡ ﾟ☾ ﾟ｡⋆" * 10)
+               print("\n", result3, "\n")
+               print("⋆｡ﾟ☁︎｡⋆｡ ﾟ☾ ﾟ｡⋆" * 10)
+               calculatorhistory_file.write(f"{number_1} {ask_usr} {number_2} = {multi_ply}" + '\n')
+               break
     # Division
-             else:
-                try:
-                   divi_sion = number_1 / number_2
-                   result4 = (f"\n\33[7m{number_1} {ask_usr} {number_2} = {divi_sion}\33[0m")
-                   print("⋆｡ﾟ☁︎｡⋆｡ ﾟ☾ ﾟ｡⋆" * 10)
-                   print("\n", result4, "\n")
-                   print("⋆｡ﾟ☁︎｡⋆｡ ﾟ☾ ﾟ｡⋆" * 10)
-                   calculatorhistory_file.write(f"{number_1} {ask_usr} {number_2} = {divi_sion}" + '\n')
-                except ZeroDivisionError:
-                    print("\n\33[1m\33[31mYou can't use zero(0) as your divisor\33[0m")
-     # If no, Display "Thank you" and exit
-          else:
-            exit_1 = Figlet(font = "slant")
-            print(colored(exit_1.renderText("Thank you! <3"), "magenta"))
-            print("\n\33[3m Until next time!... '૮₍ •⤙ •˶|\33[0m")            
+            else:
+               try:
+                  divi_sion = number_1 / number_2
+                  result4 = (f"\n\33[7m{number_1} {ask_usr} {number_2} = {divi_sion}\33[0m")
+                  print("⋆｡ﾟ☁︎｡⋆｡ ﾟ☾ ﾟ｡⋆" * 10)
+                  print("\n", result4, "\n")
+                  print("⋆｡ﾟ☁︎｡⋆｡ ﾟ☾ ﾟ｡⋆" * 10)
+                  calculatorhistory_file.write(f"{number_1} {ask_usr} {number_2} = {divi_sion}" + '\n')
+               except ZeroDivisionError:
+                  print("\n\33[1m\33[31mYou can't use zero(0) as your divisor\33[0m")
+               else:
+                  break
+            calculatorhistory_file.close()   
+            continue   
+process()
+keep_going = True
+while keep_going:
+   # ask if they wanted to continue
+   again_usr = input("\n\33[36m Would you like to try again: Enter 'Y' if yes and 'N' if no: \33[0m")
+       # If yes call process
+   if again_usr.upper() == "N":
+         exit_1 = Figlet(font = "slant")
+         print(colored(exit_1.renderText("Thank you! <3"), "magenta"))
+         print("\n\33[3m Until next time!... '૮₍ •⤙ •˶|\33[0m")
+   # If no, Display "Thank you" and exit
+   elif again_usr.upper() == "Y":
+      process()
+   else:
+      False         
 # Closing the file and program
-            calculatorhistory_file.close()
 process()
